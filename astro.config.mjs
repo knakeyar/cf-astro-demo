8348be8c0665e783a888fbd71d92eb44e8c46edd
Kice Nakeyar <knakeyar@gmail.com>
2026-05-28 21:13:30 +0000

    change config, removed workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,3 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
-});
+export default defineConfig({});
