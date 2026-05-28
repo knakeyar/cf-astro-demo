8dab89ad64fe43327bf8f1d63480a3b940ddfbab
Kice Nakeyar <knakeyar@gmail.com>
2026-05-28 20:18:43 +0000

    reverted back to astro managed cf worker

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
+
+interface Env {
+  GITHUB_CLIENT_ID: string;
+  GITHUB_CLIENT_SECRET: string;
+}
